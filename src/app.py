d2e47df7d46f47a5a6685cf4210a903302e8d3c6
MyNameIsGs <sebascarmun@gmail.com>
2023-04-07 21:57:58 +0000

    Homework done

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planets, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +37,154 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_user():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    users= User.query.all()
+    users_list=[]
+    for user in users:
+        users_list.append(user.serialize())
 
-    return jsonify(response_body), 200
+    return jsonify(users_list), 200
+
+@app.route('/user', methods=['POST'])
+def post_user():
+
+    body= request.json
+    email= body.get("email",None)
+    password= body.get("password",None)
+
+    if email is None or password is None:
+       return jsonify({"Message":"Email or Password is not valid"}), 400
+
+    else:
+        try:
+            user=User(email=email,password=password)
+            db.session.add(user)
+            db.session.commit()
+            return jsonify({"Message":"User Registered"})
+
+        except Exception as error:
+                return jsonify(error.args[0]),error.args[1]
+
+@app.route('/characters', methods=['POST'])
+def post_characters():
+
+    body= request.json
+    name= body.get("name",None)
+    birthday= body.get("birthday",None)
+    color_eyes= body.get("color_eyes",None)
+    color_hair= body.get("color_hair",None)
+    tone_skin= body.get("tone_skin",None)
+
+    if name is None or birthday is None or color_eyes is None or color_hair is None or tone_skin is None:
+        return jsonify({"Message":"Data not found"}), 400
+
+    else:
+        try:
+            character=Character(name=name, birthday=birthday, color_eyes=color_eyes, color_hair=color_hair, tone_skin=tone_skin)
+            db.session.add(character)
+            db.session.commit()
+            return jsonify({"Message":"Character Added"})
+
+        except Exception as error:
+                return jsonify(error.args[0]),error.args[1]
+
+@app.route('/planets', methods=['POST'])
+def post_planets():
+
+
+    body= request.json
+    name= body.get("name",None)
+    poblation= body.get("poblation",None)
+    diameter= body.get("diameter",None)
+    climate= body.get("climate",None)
+    
+
+    if name is None or poblation is None or diameter is None or climate is None:
+        return jsonify({"Message":"Data not found"}), 400
+
+    else:
+        try:
+            planets=Planets(name=name, poblation=poblation, diameter=diameter, climate=climate)
+            db.session.add(planets)
+            db.session.commit()
+            return jsonify({"Message":"Planet Added"})
+
+        except Exception as error:
+                return jsonify(error.args[0]),error.args[1]
+
+
+@app.route('/characters', methods=['GET'])
+def get_character():
+
+    characters= Character.query.all()
+    characters_list=[]
+    for character in characters:
+        characters_list.append(character.serialize())
+        
+@app.route('/planets', methods=['GET'])
+def get_planet():
+
+    planets= Planets.query.all()
+    planets_list=[]
+    for planet in planets:
+        planets_list.append(planet.serialize())
+
+    return jsonify(planets_list), 200
+
+@app.route('/user/favorites/<int:id>', methods=["GET"])
+def get_favorites(id):
+
+    favorites= Favorites.query.filter_by(user_id=id)
+    print(favorites)
+    favorites_list=[]
+    for favorite in favorites:
+        favorites_list.append(favorite.serialize())
+
+    return jsonify([fav.serialize() for fav in favorites]), 200
+
+@app.route('/user/favorites/<int:character_id>/<int:user_id>', methods=["POST"])
+def post_favorite_character(user_id, character_id):
+
+    body= request.json
+    name= body.get("name",None)
+    favorites= Favorites.query.filter_by(user_id=user_id, character_id=character_id).first()
+
+    if favorites is not None:
+        return jsonify ({"Message":"Already in Favorites"})
+    
+    else:
+        try:
+            newFavorite=Favorites(name=name, character_id=character_id, user_id=user_id)
+            db.session.add(newFavorite)
+            db.session.commit()
+            return jsonify({"Message":"Character Added"})
+
+        except Exception as error:
+                return jsonify(error.args[0])
+
+@app.route('/user/favorites/<int:planets_id>/<int:user_id>', methods=["POST"])
+def post_favorite_planet(user_id, planets_id):
+
+    body= request.json
+    name= body.get("name",None)
+    favorites= Favorites.query.filter_by(user_id=user_id, planets_id=planets_id).first()
+
+    if favorites is not None:
+        return jsonify ({"Message":"Already in Favorites"})
+    
+    else:
+        try:
+            newFavorite=Favorites(name=name, planets_id=planets_id, user_id=user_id)
+            db.session.add(newFavorite)
+            db.session.commit()
+            return jsonify({"Message":"Planet Added"})
+
+        except Exception as error:
+                return jsonify(error.args[0])
+  
+  
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
 
 db = SQLAlchemy()
 
@@ -6,7 +7,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+   
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -16,4 +17,59 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    birthday = db.Column(db.String(80), nullable=False)
+    color_eyes = db.Column(db.String(50), nullable=False)
+    color_hair = db.Column(db.String(50), nullable=False)
+    tone_skin = db.Column(db.String(50), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birthday": self.birthday,
+            "color_eyes": self.color_eyes,
+            "color_hair": self.color_hair,
+            "tone_skin": self.tone_skin
+        }
+
+class Planets(db.Model):
+    
+  
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    poblation = db.Column(db.String(250))
+    diameter = db.Column(db.String(250))
+    climate = db.Column(db.String(250))
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "poblation": self.poblation,
+            "diameter": self.diameter,
+            "climate": self.climate
+            
+        }
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    character_id = db.Column(db.Integer, ForeignKey("character.id"))
+    planets_id = db.Column(db.Integer, ForeignKey("planets.id"))
+    user_id = db.Column(db.Integer, ForeignKey("user.id"))
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "character_id": self.character_id,
+            "planets_id": self.planets_id,
+            "user_id": self.user_id
         }
